af163524b7944239e412f19a587544012988bd97
Kaushal Bundel <kaushalbundel@gmail.com>
2024-04-09 20:03:13 +0530

    gitignore added

--- elpa/denote-2.3.4/denote-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from denote.el  -*- no-byte-compile: t -*-
-(define-package "denote" "2.3.4" "Simple notes with an efficient file-naming scheme" '((emacs "28.1")) :commit "514a5d0d394adeaa7bc26e90f76376bf149163c8" :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Protesilaos Stavrou" . "info@protesilaos.com") :url "https://github.com/protesilaos/denote")
