6887c40cbc5b14e6dba71986a643e7e779315080
Kaushal Bundel <kaushalbundel@gmail.com>
2025-08-10 20:18:21 +0530

    mac font updated

--- extras/fonts.el
+++ extras/fonts.el
@@ -1,8 +1,8 @@
 ;; setting fonts for mac and linux
 (cond
  ((equal system-type 'darwin)
-  (set-face-attribute 'default nil :family "JetBrains Mono" :weight 'Regular :height 120)
-  (set-face-attribute 'variable-pitch nil :family "Iosevka Comfy Motion Fixed" :weight 'Regular :height 120))
+  (set-face-attribute 'default nil :family "Aporetic Sans Mono" :weight 'Regular :height 120)
+  (set-face-attribute 'variable-pitch nil :family "Aporetic Serif Mono" :weight 'Regular :height 120))
  ((equal system-type 'gnu/linux)
  (set-face-attribute 'default nil :family "Aporetic Sans Mono" :height 110)
   (set-face-attribute 'variable-pitch nil :family "Aporetic Serif Mono" :height 110))
